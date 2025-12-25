affc6a88db9fa5c6c38884309a60e6dd5967b84e
JSiggelkow <jonas.siggelkow@unitybox.de>
2025-12-25 11:12:01 +0100

    init backend

---
+++ canalysisbackend/src/main/java/de/jsi/canalysisbackend/CanalysisbackendApplication.java
@@ -0,0 +1,13 @@
+package de.jsi.canalysisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanalysisbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanalysisbackendApplication.class, args);
+	}
+
+}
---
+++ canalysisbackend/src/test/java/de/jsi/canalysisbackend/CanalysisbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jsi.canalysisbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanalysisbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
